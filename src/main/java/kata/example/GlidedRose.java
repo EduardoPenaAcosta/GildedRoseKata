a555d33fb40cc976cb4e988877acc2eae10e127a
Eduardo Peña Acosta <eduardope14@hotmail.com>
2022-04-04 12:05:41 +0100

    Todos los test realizados con bucle

--- src/main/java/kata/example/GlidedRose.java
+++ src/main/java/kata/example/GlidedRose.java
@@ -1,2 +1,25 @@
-package kata.example;public class GlidedRose {
+package kata.example;
+
+import java.util.regex.Pattern;
+
+import static java.lang.Integer.parseInt;
+
+public class GlidedRose {
+
+    protected int add(String givenText){
+
+        if(!givenText.isEmpty()){
+            int result = 0;
+            String[] splittedGivenInput = givenText.split("[,:\\n]");
+            for(String slittedInput : splittedGivenInput){
+
+                if(Pattern.matches(".*[a-zA-Z]+.*",slittedInput)) continue;
+                if(!slittedInput.isEmpty()) result += parseInt(slittedInput);
+            }
+            return result;
+        }
+
+        return 0;
+    }
+
 }
--- src/test/java/kata/example/GlidedRoseTest.java
+++ src/test/java/kata/example/GlidedRoseTest.java
@@ -1,2 +1,34 @@
-package kata.example;public class GlidedRose {
+package kata.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GlidedRoseTest {
+
+    GlidedRose gr = new GlidedRose();
+
+    @Test
+    public void ifTextIsEmpty(){
+       assertEquals(0, gr.add(""));
+    }
+
+    @Test
+    public void ifDetectsANumber(){
+        assertEquals(1, gr.add("1"));
+    }
+
+    @Test
+    public void sumArrayNumbers(){
+        assertEquals(2, gr.add("1,1"));
+    }
+
+    @Test
+    public void sumArrayNumbersWithLetters(){
+        assertEquals(7, gr.add("1,2,a,4"));
+    }
+
+    @Test
+    public void sumArrayNumbersWithLettersAndSpacers(){
+        assertEquals(7, gr.add("1,2,A,\n,4"));
+    }
 }
